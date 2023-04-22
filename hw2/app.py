094aa55b4fcee5b5596494a4c84bbba1c05faa55
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-04-21 19:44:26 -0700

    Fixed error

--- hw2/app.py
+++ hw2/app.py
@@ -14,7 +14,7 @@ app.add_url_rule('/',
 
 
 app.add_url_rule('/enter',
-                 view_func=Sign.as_view('sign'),
+                 view_func=Enter.as_view('enter'),
                  methods=["GET", "POST"])
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
