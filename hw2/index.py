30ce9df5153b1d5df7b5c2f9d9bac37bf5edf145
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-04-21 20:10:06 -0700

    Fixed typo

--- hw2/index.py
+++ hw2/index.py
@@ -5,5 +5,5 @@ import gbmodel
 class Index(MethodView):
     def get(self):
         model = gbmodel.get_model()
-        entries = [dict(bname=row[0], bcode=row[1], bfloor=row[2], closeRoomNumer=row[3], rating=row[4] ) for row in model.select()]
+        entries = [dict(bname=row[0], bcode=row[1], bfloor=row[2], closeRoomNumber=row[3], rating=row[4] ) for row in model.select()]
         return render_template('index.html', entries = entries)
