ad105855932a29bc8b75bdbbdd5e9e60049b6543
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-04-21 18:25:40 -0700

    model_sqlite3 updated

--- hw2/gbmodel/model_sqlite3.py
+++ hw2/gbmodel/model_sqlite3.py
@@ -1,4 +1,3 @@
-import sqlite3
 """
 Backend app for to store college building details
 
@@ -14,7 +13,41 @@ class model(Model):
         cursor = connection.cursor()
 
         try:
-            cursor.execute("select cout(rowid) from building")
+            cursor.execute("select count(rowid) from building")
         
         except sqlite3.OperationalError:
             cursor.execute("create table building (bname text, bcode text, bfloor integer, closeRoomNumber integer, rating real )")
+
+        cursor.close()
+
+    def select(self):
+        """
+        This is used to get all the rows from the database
+        Each row will have: Building name, Building code, building floor, Closest Room Number and rating.
+        :return: List of lists containing all rows of database
+        """
+
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM building")
+        return cursor.fetchall()
+
+    def insert(self, bname, bcode, bfloor, closeRoomNumber, rating):
+        """
+        Inserts entries to database
+        :param bname: String
+        :param bcode: String
+        :param bfloor: Int
+        :param closeRoomNumber: Int
+        :param rating: Float
+        :return: True
+        :raises: Database error on connection and insertion
+        """
+
+        params = {'bname':bname, 'bcode':bcode, 'bfloor':bfloor, 'closeRoomNumber':closeRoomNumber, 'rating':rating}
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("insert into building (bname, bcode, bfloor, closeRoomNumber, rating) VALUES(:bname, :bcode, :bfloor, :closeRoomNumber, :rating)", params)
+        connection.commit()
+        cursor.close()
+        return True
