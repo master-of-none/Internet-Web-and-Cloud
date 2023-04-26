67e8e95853a8616d1675721ab2f8716860508261
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-04-26 12:22:02 -0700

    Added docstrings and some comments.

--- hw2/app.py
+++ hw2/app.py
@@ -1,13 +1,19 @@
 """
 Building details built using Flask
 """
+
+#import required things.
 import flask
 from flask.views import MethodView
 from index import Index
 from enter import Enter
 from view import View
+
+#intialise flask.
 app = flask.Flask(__name__)
 
+
+# Add routes such that homepage, view and insert is acheived.
 app.add_url_rule('/',
                  view_func=Index.as_view('index'),
                  methods=["GET"])
@@ -18,5 +24,7 @@ app.add_url_rule('/view',
 app.add_url_rule('/enter',
                  view_func=Enter.as_view('enter'),
                  methods=["GET", "POST"])
+
+# Run the program on specified port and on localhost
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- hw2/enter.py
+++ hw2/enter.py
@@ -1,12 +1,26 @@
+"""
+This file is used to enter the values to database.
+"""
+
+#Import required things.
 from flask import redirect, request, url_for, render_template
 from flask.views import MethodView
 import gbmodel
 
 class Enter(MethodView):
     def get(self):
+        """
+        This function is used to render the enter.html template
+        :return: enter.html file which contains front end to enter the details to database.
+        """
         return render_template('enter.html')
 
     def post(self):
+        """
+        This function basically inserts the valye to database, this executes when data is entered and enter
+        button is clicked.
+        :return: After entering, this will return the homepage of the application.
+        """
         model = gbmodel.get_model()
         model.insert(request.form['name'], request.form['code'], request.form['floor'], request.form['closeRoomNumber'], request.form['rating'])
         return redirect(url_for('index'))
