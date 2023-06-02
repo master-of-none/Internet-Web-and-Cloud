9b18f475f11141fd0ef5cc3a0d8e5ffe695cd59b
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-02 13:08:49 -0700

    changes in api

--- final/enter.py
+++ final/enter.py
@@ -2,6 +2,7 @@ from flask import redirect, request, url_for, render_template, Flask
 from flask.views import MethodView
 import requests
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -14,6 +15,10 @@ class Enter(MethodView):
         EDAMAM_API_ID = "ec4e4386"
         EDAMAM_API_KEY = "ff18d36a899d9fe39b9e4be092e30e02"
 
+    
+        EDAMAM_API_ID1 = "012b51a7"
+        EDAMAM_API_KEY1 = "fd4db16491f7dd7864d821e2b8645900"
+        
         # Spoonacular API key
         SPOONACULAR_API_KEY = "279e119500fd40b3bca3dd8e6a3ef7aa"
 
@@ -21,75 +26,47 @@ class Enter(MethodView):
         def analyze_nutrients(ingredient):
             url = "https://api.edamam.com/api/nutrition-data"
             params = {
-                "app_id": EDAMAM_API_ID,
-                "app_key": EDAMAM_API_KEY,
+                "app_id": EDAMAM_API_ID1,
+                "app_key": EDAMAM_API_KEY1,
                 "ingr": ingredient,
                 "to": 1
             }
             response = requests.get(url, params=params)
             data = response.json()
+
+            with open("data1.json", "w") as file:
+                json.dump(data, file)
+
             nutrients = data.get("totalNutrients", {})
             return nutrients
 
-        # Function to search recipes using Edamam API
+        
         def search_recipes(ingredients):
-            url = "https://api.edamam.com/api/recipes/v2"
-            params = {
-                "type": "public",
-                "q": ",".join(ingredients),
-                "app_id": EDAMAM_API_ID,
-                "app_key": EDAMAM_API_KEY,
-                "limit": 1  # Number of recipes to retrieve
-            }
-            response = requests.get(url, params=params)
-            data = response.json()
-            hits = data.get("hits")
-            if hits:
-                return hits[:1]  # Return only the first recipe
-            else:
-                return []
-
-        # Function to get nutritional info using Spoonacular API
-        def get_nutritional_info(recipe_url):
-            url = "https://api.spoonacular.com/recipes/extract"
+            url = "https://api.spoonacular.com/recipes/findByIngredients"
             params = {
                 "apiKey": SPOONACULAR_API_KEY,
-                "url": recipe_url
+                "ingredients": ingredients,
+                "number": 1
             }
             response = requests.get(url, params=params)
             data = response.json()
-            nutrients = data.get("nutrition", {}).get("nutrients", [])
-            return data
 
+            with open("data.json", "w") as file:
+                json.dump(data, file)
+
+            original_lines = [ingredient["original"] for ingredient in data[0]["usedIngredients"] + data[0]["missedIngredients"]]
+            original_text = "\n".join(original_lines)
+            return original_text
         # Your code goes here
+        
         ingredients = request.form.get("ingredients").split(",")
-
-        # Search recipes
+        
+        # # Search recipes
         recipes = search_recipes(ingredients)
-        recipe_info = []
-
-        # Analyze nutrients for each recipe
-        for recipe in recipes:
-            recipe_url = recipe["recipe"]["url"]
-            nutritional_info = get_nutritional_info(recipe_url)
-            ingredients = nutritional_info.get("extendedIngredients", [])
-            nutrients = []
-            for ingredient in ingredients:
-                nutrient = analyze_nutrients(ingredient["name"])
-                nutrients.append({
-                    "name": ingredient["name"],
-                    "amount": ingredient.get("amount", ""),
-                    "unit": ingredient.get("unit", ""),
-                    "nutrient": nutrient
-                })
-            recipe_info.append({
-                "title": recipe["recipe"]["label"],
-                "image": recipe["recipe"]["image"],
-                "url": recipe_url,
-                "nutrients": nutrients
-            })
+        # recipe_info = []
+        print(recipes)
     
-        return render_template("enter.html", recipes=recipe_info)
+        return render_template("enter.html")
         
 
 
