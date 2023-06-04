041eb1454804e5fe2e6aa205419ed8b756f0a1da
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-04 09:47:53 -0700

    Initial Commit for database

---
+++ final/gbmodel/Model.py
@@ -0,0 +1,23 @@
+
+class Model():
+    def select(self):
+        """
+        This is used to extract all the entries from database.
+
+        This will return the entries from buildingDB
+        Each row will have: Building Name, Building Code, Building Floor, Closest Room Number, Rating
+        return: List, listing all the rows of the database.
+        """
+        pass
+
+    def insert(self, bName, bCode, bFloor, closeRoomNumber, rating):
+        """
+        Insert all the values into the building database.
+        :param bName: String
+        :param bCode: String
+        :param bFloor: Int
+        :param closeRoomNumber: Int
+        :param rating: Float
+        """
+        pass
+
